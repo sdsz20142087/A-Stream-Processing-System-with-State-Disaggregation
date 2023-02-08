803321ba29e6fe85b0ec62be3fd17a5cb245833c
Yuanli Wang <yuanliw@bu.edu>
2023-02-08 20:16:31 +0000

    Update dataflow/src/main/java/operators/SingleInputOperator.java

--- dataflow/src/main/java/operators/SingleInputOperator.java
+++ dataflow/src/main/java/operators/SingleInputOperator.java
@@ -3,19 +3,10 @@ package operators;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public abstract class SingleInputOperator<IN, OUT> extends Thread {
-    //TODO output queue where it can push results
     /**
      * Apply transformation
      * @param element input event
      */
-
-//    private ConcurrentLinkedQueue<IN> input;
-//    private ConcurrentLinkedQueue<OUT> output;
-//
-//    public SingleInputOperator(ConcurrentLinkedQueue<IN> input, ConcurrentLinkedQueue<OUT> output){
-//        this.input=input;
-//        this.output=output;
-//    }
 
     public abstract void processElement(IN input);
 
