8d088d8e588d142274d20c4e358611b00ad225f4
tzyuan <tzy15368@outlook.com>
2023-02-20 00:32:38 -0500

    add grpc dependencies

---
+++ src/main/java/controller/ControlPlane.java
@@ -0,0 +1,11 @@
+package controller;
+
+import pb.Cp;
+import pb.Tm;
+
+public class ControlPlane {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"+Cp.class.getName()+ Tm.class.getName());
+
+    }
+}
