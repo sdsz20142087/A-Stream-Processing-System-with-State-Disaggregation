87c9e8a269836d72a820fae10d5bef5f5c693492
Yingzhe Dong <yzdong99@bu.edu>
2023-02-24 03:00:40 -0500

    fix bug

--- src/main/DB/etcd_init.sh
+++ src/main/DB/etcd_init.sh
@@ -1,8 +1,13 @@
+if docker ps -a --format '{{.Names}}' | grep -q '^etcd-gcr-v3.4.24$'; then
+    docker rm etcd-gcr-v3.4.24
+else
+    echo "Container not found"
+fi
 rm -rf /tmp/etcd-data.tmp && mkdir -p /tmp/etcd-data.tmp && \
 docker rmi gcr.io/etcd-development/etcd:v3.4.24 || true && \
 docker run \
-  -p 2379:2379 \
-  -p 2380:2380 \
+  -p 8179:2379 \
+  -p 8180:2380 \
   --mount type=bind,source=/tmp/etcd-data.tmp,destination=/etcd-data \
   --name etcd-gcr-v3.4.24 \
   gcr.io/etcd-development/etcd:v3.4.24 \
