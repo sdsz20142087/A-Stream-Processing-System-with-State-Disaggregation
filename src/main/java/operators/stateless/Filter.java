b922205f8d9422a36fccaf68fc36c10c9c7bebfa
JerryW35 <wzr@bu.edu>
2023-03-01 18:40:04 -0500

    add filter operator

---
+++ src/main/java/operators/stateless/Filter.java
@@ -0,0 +1,41 @@
+package operators.stateless;
+
+import java.io.Serializable;
+import java.util.function.Predicate;
+
+import com.google.protobuf.ByteString;
+import exec.SerDe;
+import pb.Op;
+import operators.BaseOperator;
+
+public class Filter<T> extends BaseOperator implements Serializable {
+    private Predicate<T> predicate;
+    private SerDe<T> serde;
+    public Filter(Op.OperatorConfig config) {
+        super(config);
+        this.predicate = UDFpredicate;
+
+    }
+    @Override
+    public void run(){
+        super.run();
+    }
+    @Override
+    protected void processElement(ByteString in) {
+        T data = serde.deserialize(in);
+        if (predicate.test(data)) {
+            sendOutput(Op.Msg.newBuilder().setType(Op.Msg.MsgType.DATA).setData(in).build());
+        }
+    }
+
+    // need to be implemented by the user
+    Predicate<T> UDFpredicate = new Predicate<T>() {
+        @Override
+        public boolean test(T t) {
+            return false;
+        }
+    };
+
+
+
+}
