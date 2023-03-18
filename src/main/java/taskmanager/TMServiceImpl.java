cf196481f6d760ff7fbc8a73f110d8391a150be6
tzyuan <tzy15368@outlook.com>
2023-03-18 02:46:31 -0400

    refactor OP/TM to share one send loop

--- src/main/java/taskmanager/TMServiceImpl.java
+++ src/main/java/taskmanager/TMServiceImpl.java
@@ -2,6 +2,7 @@ package taskmanager;
 
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
+import kotlin.Pair;
 import operators.BaseOperator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,18 +13,38 @@ import utils.TMException;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
 
 class TMServiceImpl extends TMServiceGrpc.TMServiceImplBase {
-    private int operatorQuota;
-    private HashMap<String, BaseOperator> operators;
-    private Logger logger = LogManager.getLogger();
+    private final int operatorQuota;
+    private final HashMap<String, BaseOperator> operators;
+    private final Logger logger = LogManager.getLogger();
+
+    // map of< TM's address, PushMsgClient>
+    private final Map<String, PushMsgClient> pushMsgClients = new HashMap<>();
+
+    private final Map<String, LinkedBlockingQueue<Tm.Msg>> opInputQueues = new HashMap<>();
+
+    // map of <operator name, message>
+    private final LinkedBlockingQueue<Pair<String, Tm.Msg>> msgQueue = new LinkedBlockingQueue<>();
 
     public TMServiceImpl(int operatorQuota) {
         super();
         this.operatorQuota = operatorQuota;
         operators = new HashMap<>();
         logger.info("TM service started with operator quota: " + operatorQuota);
+        // boot the send loop
+        new Thread(() -> {
+            try {
+                this.sendLoop();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
     }
 
     public void getStatus(Tm.TMStatusRequest request,
@@ -38,8 +59,34 @@ class TMServiceImpl extends TMServiceGrpc.TMServiceImplBase {
     /**
      *
      */
+
+    // the control plane sends over serialized operator,
+    // we can just deserialize it and add it to the operators map
+    private void initOperator(Tm.AddOperatorRequest request) throws IOException, ClassNotFoundException {
+        byte[] bytes = request.getObj().toByteArray();
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        ObjectInputStream ois;
+
+        ois = new ObjectInputStream(bis);
+        BaseOperator op = (BaseOperator) ois.readObject();
+        LinkedBlockingQueue<Tm.Msg> inputQueue = new LinkedBlockingQueue<>();
+        this.opInputQueues.put(op.getName(), inputQueue);
+        // the queues must be initialized before the operator starts
+        op.init(inputQueue, msgQueue);
+        op.start();
+        logger.info(String.format("Started operator %s", op.getName()));
+        operators.put(op.getName(), op);
+
+        // initialize the operator's pushmsg client if needed
+        for(String addr: request.getConfig().getOutputAddressList()){
+            if(!pushMsgClients.containsKey(addr)){
+                pushMsgClients.put(addr, new PushMsgClient(logger, addr));
+            }
+        }
+    }
+
     @Override
-    public void addOperator(Tm.AddOperatorRequest request,
+    public synchronized void addOperator(Tm.AddOperatorRequest request,
                             StreamObserver<Empty> responseObserver) {
         if (operators.size() >= operatorQuota) {
             responseObserver.onError(new TMException("operator quota exceeded"));
@@ -50,23 +97,30 @@ class TMServiceImpl extends TMServiceGrpc.TMServiceImplBase {
             return;
         }
         logger.info(String.format("adding operator %d/%d",this.operators.size()+1,this.operatorQuota));
-        // TODO: add operator, the control plane sends over serialized operator so
-        // we can just deserialize it and add it to the operators map
-        byte[] bytes = request.getObj().toByteArray();
-        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-        ObjectInputStream ois;
-        BaseOperator op;
-        try {
-            ois = new ObjectInputStream(bis);
-            op = (BaseOperator) ois.readObject();
+        try{
+            initOperator(request);
         } catch (IOException | ClassNotFoundException e) {
-            responseObserver.onError(e);
+            responseObserver.onError(new TMException("failed to deserialize operator"));
             return;
         }
+        responseObserver.onNext(Empty.getDefaultInstance());
+        responseObserver.onCompleted();
+    }
 
-        op.start();
-        operators.put(op.getName(), op);
-
+    @Override
+    public synchronized void pushMsg(Tm.Msg request, StreamObserver<Empty> responseObserver) {
+        logger.info("got pushMsg request");
+        String opName = request.getOperatorName();
+        if(!operators.containsKey(opName)){
+            responseObserver.onError(new TMException("operator not found"));
+            return;
+        }
+        try {
+            opInputQueues.get(opName).put(request);
+        } catch (InterruptedException e) {
+            responseObserver.onError(new TMException("failed to push message to operator"));
+            return;
+        }
         responseObserver.onNext(Empty.getDefaultInstance());
         responseObserver.onCompleted();
     }
@@ -84,5 +138,34 @@ class TMServiceImpl extends TMServiceGrpc.TMServiceImplBase {
     public void reConfigOperator(Tm.ReConfigOperatorRequest request,
                                  StreamObserver<Empty> responseObserver) {
 
+    }
+
+    private void sendLoop() throws InterruptedException {
+        while (true) {
+            Pair<String, Tm.Msg> msg = msgQueue.take();
+            String opName = msg.getFirst();
+            Tm.OperatorConfig config = operators.get(opName).getConfig();
+            List<String> targetAddr = new ArrayList<>();
+            // apply the partition strategy
+            switch (config.getPartitionStrategy()) {
+                case ROUND_ROBIN:
+                    targetAddr.add(config.getOutputAddressList().get(0));
+                    break;
+                case HASH:
+                    // ???
+
+                    // FIXME: WHAT DOES THIS EVEN MEAN? targetStub= stubs.get(msg.getPartitionId());
+                    break;
+                case BROADCAST:
+                    targetAddr = config.getOutputAddressList();
+                    break;
+                case RANDOM:
+                    // ???
+                    break;
+            }
+            for(String addr: targetAddr){
+                pushMsgClients.get(addr).pushMsg(msg.getSecond());
+            }
+        }
     }
 }
--- src/main/java/taskmanager/TaskManager.java
+++ src/main/java/taskmanager/TaskManager.java
@@ -1,27 +1,10 @@
 package taskmanager;
 
-import com.google.protobuf.ByteString;
-import com.google.protobuf.Empty;
 import config.Config;
 import config.TMConfig;
-import exec.SerDe;
 import io.grpc.*;
-import io.grpc.stub.StreamObserver;
-import operators.BaseOperator;
-import operators.ISource;
-import operators.SinkOperator;
-import operators.SourceOperator;
-import pb.Op;
-import pb.Tm;
 import utils.NodeBase;
-import utils.StringSerde;
-import utils.WikiFileSource;
-
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class TaskManager extends NodeBase {
     private static TaskManager instance;
@@ -36,14 +19,17 @@ public class TaskManager extends NodeBase {
         Config.LoadConfig(configPath);
 
         tmcfg = Config.getInstance().taskManager;
+
+        // !!!!tm_port is assigned in runtime to make things simple
         tmcfg.tm_port = grpcPort;
+
         logger.info("tm_port=" + Config.getInstance().taskManager.tm_port);
         registryClient = new RegistryClient(tmcfg.cp_host, tmcfg.cp_port, tmcfg.tm_port);
         tmService = new TMServiceImpl(tmcfg.operator_quota);
         tmServer = ServerBuilder.forPort(tmcfg.tm_port).addService(tmService).build();
     }
 
-    public void start() {
+    public void init() {
         try {
             // register at control plane
             logger.info("Registering at control plane=" + tmcfg.cp_host + ":" + tmcfg.cp_port);
@@ -52,67 +38,6 @@ public class TaskManager extends NodeBase {
             this.tmServer.start();
             logger.info("TaskManager started on " + tmcfg.tm_port);
             // let this thread block until server termination
-
-            // simulate control plane requests
-            // ------------------------------------------
-            Thread.sleep(1000);
-            logger.info("starting ops simulation");
-            ArrayList<String> addrs = new ArrayList<>();
-            addrs.add("127.0.0.1:9002");
-            Op.OperatorConfig cfgSink = Op.OperatorConfig.newBuilder()
-                    .setName("sink-1-test")
-                    .setType(Op.OperatorType.SINK)
-                    .setListenPort(9002)
-                    .setPartitionStrategy(Op.PartitionStrategy.ROUND_ROBIN)
-                    .build();
-            Op.OperatorConfig cfgSource = Op.OperatorConfig.newBuilder()
-                    .setName("source-1-test")
-                    .setType(Op.OperatorType.SOURCE)
-                    .setListenPort(9000)
-                    .addAllNextOperatorAddress(addrs)
-                    .setPartitionStrategy(Op.PartitionStrategy.ROUND_ROBIN)
-                    .build();
-            ISource<String> src = new WikiFileSource("data.txt");
-            SerDe<String> serde = new StringSerde();
-            SourceOperator<String> sourceOperator = new SourceOperator<String>(cfgSource, src, serde);
-            SinkOperator sink = new SinkOperator(cfgSink);
-
-            LinkedList<BaseOperator> ops = new LinkedList<>();
-            ops.add(sink);
-            ops.add(sourceOperator);
-            for(BaseOperator op : ops) {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                ObjectOutputStream oos = new ObjectOutputStream(baos);
-                oos.writeObject(op);
-                byte[] bytes = baos.toByteArray();
-                ByteString bs = ByteString.copyFrom(bytes);
-                logger.info("got operator "+op.getName()+" bytes, size=" + bytes.length + "");
-
-                Tm.AddOperatorRequest req = Tm.AddOperatorRequest
-                        .newBuilder()
-                        .setConfig(cfgSource)
-                        .setObj(bs)
-                        .build();
-                tmService.addOperator(req, new StreamObserver<Empty>() {
-                    @Override
-                    public void onNext(Empty empty) {
-                    }
-
-                    @Override
-                    public void onError(Throwable throwable) {
-                        logger.error("add operator error", throwable);
-                    }
-
-                    @Override
-                    public void onCompleted() {
-                        logger.info("add operator completed");
-                    }
-                });
-                logger.info("sleeping 2");
-                Thread.sleep(2000);
-                logger.info("woke up");
-            }
-            // ------------------------------------------
 
             this.tmServer.awaitTermination();
         } catch (IOException | InterruptedException e) {
@@ -128,6 +53,6 @@ public class TaskManager extends NodeBase {
     }
 
     public static void main(String[] args) {
-        TaskManager.getInstance().start();
+        TaskManager.getInstance().init();
     }
 }
