6f320cd4f38669fffee4da80b07f1bec34aeb69b
tzyuan <tzy15368@outlook.com>
2023-03-24 23:30:17 -0400

    implement dequeproxy with tests

--- src/main/java/stateapis/ListStateAccessor.java
+++ src/main/java/stateapis/ListStateAccessor.java
@@ -1,9 +1,15 @@
 package stateapis;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class ListStateAccessor<T> extends BaseStateAccessor<IDataflowDeque<T>> {
+
+    private DequeProxy<T> dequeProxy;
 
     public ListStateAccessor(String descriptorName, KVProvider kvProvider) {
         super(descriptorName, kvProvider);
+        this.dequeProxy = new DequeProxy<>(descriptorName, kvProvider);
     }
 
     /*
@@ -11,20 +17,23 @@ public class ListStateAccessor<T> extends BaseStateAccessor<IDataflowDeque<T>> {
      */
     @Override
     public IDataflowDeque<T> value() {
-        return new DequeProxy<>(descriptorName, this.kvProvider);
+        return this.dequeProxy;
     }
 
     @Override
     /*
-    update re-writes the entire new list, equivalent to PUT
+    update consumes the value, re-writes the entire new list, equivalent to PUT
      */
     public void update(IDataflowDeque<T> value) {
-        // TODO: implement this
+        this.dequeProxy.clear();
+        for (int i = 0; i < value.size(); i++) {
+            this.dequeProxy.addLast(value.removeFirst());
+        }
     }
 
     @Override
     public void clear() {
-        // TODO: implement this
+        this.dequeProxy.clear();
     }
 
 }
@@ -34,53 +43,114 @@ class DequeProxy<T> implements IDataflowDeque<T> {
     private String keyBase;
     private KVProvider kvProvider;
 
+    private String frontIndexKey;
+    private String sizeKey;
+    private final Logger logger = LogManager.getLogger();
+
     public DequeProxy(String keyBase, KVProvider kvProvider) {
         this.keyBase = keyBase;
         this.kvProvider = kvProvider;
+        this.frontIndexKey = keyBase + ".front";
+        this.sizeKey = keyBase + ".size";
+        if (kvProvider.listKeys(keyBase).size() == 0) {
+            kvProvider.put(frontIndexKey, 0);
+            kvProvider.put(sizeKey, 0);
+        }
+    }
+
+    private int getFrontIndex() {
+        return (int) kvProvider.get(frontIndexKey, 0);
+    }
+
+    private void setFrontIndex(int index) {
+        kvProvider.put(frontIndexKey, index);
+    }
+
+    private void setSize(int size) {
+        kvProvider.put(sizeKey, size);
+    }
+
+    private String makeIndex(int idx) {
+        return keyBase + "." + idx;
     }
 
     @Override
     public void addFirst(T value) {
-
+        int newFrontIndex = getFrontIndex() - 1;
+        kvProvider.put(makeIndex(newFrontIndex), value);
+        setSize(size() + 1);
+        setFrontIndex(newFrontIndex);
     }
 
     @Override
     public void addLast(T value) {
-
+        int nextIndex = getFrontIndex() + size();
+        kvProvider.put(makeIndex(nextIndex), value);
+        setSize(size() + 1);
     }
 
     @Override
     public T removeFirst() {
-        return null;
+        if (this.isEmpty()) {
+            throw new RuntimeException("Cannot remove from an empty deque");
+        }
+        int frontIndex = getFrontIndex();
+        logger.info("frontIndex: " + makeIndex(frontIndex));
+        T value = (T) kvProvider.get(makeIndex(frontIndex), null);
+        kvProvider.delete(makeIndex(frontIndex));
+        setFrontIndex(frontIndex + 1);
+        setSize(size() - 1);
+        return value;
     }
 
     @Override
     public T removeLast() {
-        return null;
+        if (this.isEmpty()) {
+            throw new RuntimeException("Cannot remove from an empty deque");
+        }
+        int lastIndex = getFrontIndex() + size() - 1;
+        T value = (T) kvProvider.get(makeIndex(lastIndex), null);
+        kvProvider.delete(makeIndex(lastIndex));
+        setSize(size() - 1);
+        return value;
     }
 
     @Override
     public T peekFirst() {
-        return null;
+        if(size()==0){
+            return null;
+        }
+        int frontIndex = getFrontIndex();
+        return (T) kvProvider.get(makeIndex(frontIndex), null);
     }
 
     @Override
     public T peekLast() {
-        return null;
+        if(size()==0){
+            return null;
+        }
+        int lastIndex = getFrontIndex() + size() - 1;
+        return (T) kvProvider.get(makeIndex(lastIndex), null);
     }
 
     @Override
     public void clear() {
-
+        int frontIndex = getFrontIndex();
+        int size = size();
+        for (int i = frontIndex; i < size; i++) {
+            kvProvider.delete(makeIndex(i));
+        }
+        setFrontIndex(0);
+        setSize(0);
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return (int) kvProvider.get(sizeKey, 0);
     }
 }
--- src/test/java/stateapis/DequeProxyTest.java
+++ src/test/java/stateapis/DequeProxyTest.java
@@ -1,4 +1,114 @@
+package stateapis;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DequeProxyTest {
-  
+
+    private DequeProxy<String> d;
+
+    @BeforeEach
+    void setUp() {
+        d = new DequeProxy<>("test-keybase", new LocalKVProvider("test.db"));
+        d.clear();
+    }
+
+    @Test
+    void addFirst() {
+        d.addFirst("a");
+        d.addFirst("b");
+        d.addFirst("c");
+        assertEquals(d.size(), 3);
+        assertEquals(d.peekFirst(), "c");
+    }
+
+    @Test
+    void addLast() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        assertEquals(d.peekLast(), "c");
+    }
+
+    @Test
+    void removeFirst() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        assertEquals(d.removeFirst(), "a");
+        assertEquals(d.size(), 2);
+        assertEquals(d.removeFirst(), "b");
+        assertEquals(d.size(), 1);
+        assertEquals(d.removeFirst(), "c");
+        assertEquals(d.size(), 0);
+    }
+
+    @Test
+    void removeLast() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        assertEquals(d.removeLast(), "c");
+        assertEquals(d.size(), 2);
+        assertEquals(d.removeLast(), "b");
+        assertEquals(d.size(), 1);
+        assertEquals(d.removeLast(), "a");
+        assertEquals(d.size(), 0);
+    }
+
+    @Test
+    void peekFirst() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        assertEquals(d.peekFirst(), "a");
+
+    }
+
+    @Test
+    void peekLast() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        assertEquals(d.peekLast(), "c");
+    }
+
+    @Test
+    void clear() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        d.clear();
+        assertEquals(d.size(), 0);
+    }
+
+    @Test
+    void isEmpty() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        assertFalse(d.isEmpty());
+        d.clear();
+        assertEquals(d.size(), 0);
+        assertTrue(d.isEmpty());
+    }
+
+    @Test
+    void size() {
+        d.addLast("a");
+        d.addLast("b");
+        d.addLast("c");
+        assertEquals(d.size(), 3);
+        d.clear();
+        assertEquals(d.size(), 0);
+    }
 }
